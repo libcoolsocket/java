a7ba179d5830aaaf2c43326a657d9ba966db9bf2
Veli Tasali <veli.tasali@gmail.com>
2020-08-04 09:46:42 +0300

    Add config class

--- src/main/java/com/genonbeta/coolsocket/config/Config.java
+++ src/main/java/com/genonbeta/coolsocket/config/Config.java
@@ -1,5 +1,12 @@
 package com.genonbeta.coolsocket.config;
 
-public class Config
+/**
+ * Stores info about the protocol and the library.
+ */
+public final class Config
 {
+    /**
+     * The version number of the protocol.
+     */
+    public static final int PROTOCOL_VERSION = 1;
 }
